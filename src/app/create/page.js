fcedaad98473d1a3201e992d3ebd03cb7798ac26
Naseungil <sktmddlf505@naver.com>
2024-11-13 15:23:10 +0900

    전송페이지

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,39 @@
-export default function Home() {
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Create() {
+  const router = useRouter();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, body }), //object->json
+    };
+    fetch("http://localhost:9999/topics", options)
+      .then((res) => res.json()) //결과를 객체형식으로 바꿔줌
+      .then((result) => {
+        console.log(result);
+        router.push(`/read/${result.id}`);
+        router.refresh();
+      });
+  };
   return (
     <div>
-      <p>Create !</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="title" placeholder="title" />
+        </div>
+        <div>
+          <textarea name="body" placeholder="content"></textarea>
+        </div>
+        <button type="submit">전송</button>
+      </form>
+      <hr />
     </div>
   );
 }
